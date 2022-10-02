e0db22adfdcf10243be2da74a806796d7cd1b4f7
mishankoGO <arbuzofffffff@yandex.ru>
2022-10-02 18:09:47 +0300

    added one page webservice

--- webservice/webservice.go
+++ webservice/webservice.go
@@ -8,7 +8,7 @@ import (
 func MessageHandler(news []DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		for _, article := range news {
-			fmt.Fprintf(w, "ID : %d\nDatetime : %v\nDescription : %s\nLink : %s\nPage file : %s\n", article.Id, article.Datetime, article.Description, article.Link, article.PageHtml)
+			fmt.Fprintf(w, "ID : %d\nDatetime : %v\nDescription : %s\nLink : %s\nPage file : %s\n\n", article.Id, article.Datetime, article.Description, article.Link, article.PageHtml)
 		}
 	})
 }
