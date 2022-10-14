88d3ba000b91daf1781823dd00bc554f563b4e68
Andrei Dubov <av.dubov@yandex.ru>
2022-10-14 21:08:52 +0400

    replace webservice

--- cmd/webservice/main.go
+++ cmd/webservice/main.go
@@ -1,5 +1,14 @@
-package webservice
+package main
+
+import (
+	"fmt"
+
+	"github.com/indikator/aggregator_lets_go/internal/webservice"
+	"github.com/indikator/aggregator_lets_go/model"
+)
 
 func main() {
-
+	arcticles := make([]model.Article, 0)
+	webservice.RunServer(arcticles)
+	fmt.Println(arcticles)
 }
