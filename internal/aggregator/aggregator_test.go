9f11229edac69b03c91a24aa5e98b292c3ed13a7
Albert Hamidullin <hamidullin@mxm.email>
2022-11-04 20:30:51 +0300

    #86 fix previous incorrect main merge/revert

---
+++ internal/aggregator/aggregator_test.go
@@ -0,0 +1,101 @@
+package aggregator
+
+import (
+	"testing"
+
+	"github.com/indikator/aggregator_lets_go/internal/config"
+)
+
+const (
+	configData = `# Project Aggregator YAML
+aggregator:
+  nothing:
+
+database:
+  name: stub
+  url: stub://localhost:22222/
+
+webservice:
+  port: 8080
+
+parsers:
+- stub:
+    url: https://stub.com`
+)
+
+func TestWorkWithStubParser(t *testing.T) {
+
+	c := config.NewConfig()
+
+	err := c.SetData([]byte(configData))
+
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+
+	err = c.Read()
+
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+
+	if len(c.Parsers) != 1 {
+		t.Errorf("incorrect config parsers count %d, expected %d", len(c.Parsers), 1)
+	}
+
+	parsers, err := GetParsers(c.Parsers)
+
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+
+	if len(parsers) != 1 {
+		t.Errorf("incorrect parsers count %d, expected %d", len(parsers), 1)
+	}
+
+	parser := parsers[0]
+
+	articles, err := parser.ParseAll()
+
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+
+	if len(articles) != 3 {
+		t.Errorf("incorrect articles count %d, expected %d", len(articles), 3)
+	}
+
+	db, err := GetDb(c.Database)
+
+	// if db.As(db_stub.Db) {
+	// 	t.Errorf("incorrect dbms, expected stub")
+	// }
+
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+
+	a := NewAggregator()
+
+	err = a.Init(&c.Aggregator, parsers, db)
+
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+
+	err = a.Execute()
+
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+
+	dbArticles, err := db.ReadAllArticles()
+
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+
+	if len(dbArticles) != 3 {
+		t.Errorf("incorrect db articles count %d, expected %d", len(dbArticles), 3)
+	}
+}
---
+++ internal/config/config.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	aggregator "github.com/indikator/aggregator_lets_go/internal/aggregator/config"
+	"github.com/indikator/aggregator_lets_go/internal/db"
+	"github.com/indikator/aggregator_lets_go/internal/parser"
+	"github.com/indikator/aggregator_lets_go/internal/webservice"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	data       []byte
+	Aggregator aggregator.Config
+	Database   db.Config
+	WebService webservice.Config
+	Parsers    []parser.Config
+}
+
+func NewConfig() *Config {
+	return &Config{}
+}
+
+type parserYamlConfig struct {
+	Url string `yaml:"url"`
+}
+
+type yamlConfig struct {
+	Database   db.Config                     `yaml:"database"`
+	WebService webservice.Config             `yaml:"webservice"`
+	Parsers    []map[string]parserYamlConfig `yaml:"parsers"`
+}
+
+func (c *Config) SetDataFromFile(fileName string) error {
+	data, err := os.ReadFile(fileName)
+
+	if err != nil {
+		return err
+	}
+
+	return c.SetData(data)
+}
+
+func (c *Config) SetData(data []byte) error {
+	c.data = data
+	return nil
+}
+
+func (c *Config) Read() (err error) {
+	if len(c.data) == 0 {
+		return fmt.Errorf("data to read not found")
+	}
+
+	var yc yamlConfig
+
+	err = yaml.Unmarshal(c.data, &yc)
+
+	if err != nil {
+		return
+	}
+
+	c.Database = yc.Database
+	c.WebService = yc.WebService
+	c.Parsers = make([]parser.Config, len(yc.Parsers))
+
+	for i, v := range yc.Parsers {
+		for name, p := range v {
+			c.Parsers[i] = parser.Config{Name: name, Url: p.Url}
+		}
+	}
+
+	return nil
+}
